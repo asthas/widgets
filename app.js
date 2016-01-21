9a653e9d2d271db905f6073308c3eb4a33297121
Astha Sharma <a@astha.me>
2016-01-22 00:38:42 +0530

    Added communication between directives

--- app.js
+++ app.js
@@ -5,8 +5,9 @@
 				restrict: 'E', 
 				transclude: true,
 				template: '<h2>Hello World!<h2> <div role="tabpanel" ng-transclude></div>',
+				require: '^tabset',
 				scope: {},
-				link: function(scope, elem, attr) {}
+				link: function(scope, elem, attr, tabsetCtrl) {}
 			}
 		})
 		.directive('tabset', function() {
