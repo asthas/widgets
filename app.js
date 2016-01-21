109c3ab246a7343a830452bd3ee9c248c91960a8
Astha Sharma <a@astha.me>
2016-01-22 00:56:12 +0530

    Used scope to add heading

--- app.js
+++ app.js
@@ -6,7 +6,9 @@
 				transclude: true,
 				template: '<h2>Hello World!<h2> <div role="tabpanel" ng-transclude></div>',
 				require: '^tabset',
-				scope: {},
+				scope: {
+					heading: '@'
+				},
 				link: function(scope, elem, attr, tabsetCtrl) {
 					tabsetCtrl.addTab(scope);
 				}
